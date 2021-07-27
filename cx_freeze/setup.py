da5634804d8f6235d8f3dc1053031cd058fd0551
1over137 <1over137@anche.no>
2021-07-27 08:52:09 +0800

    Update requirements

--- cx_freeze/setup.py
+++ cx_freeze/setup.py
@@ -5,7 +5,7 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
 build_exe_options = {"packages": ["ssmtool", "setuptools", "PyQt5",
-"pymorphy2_dicts", "pymorphy2", "bs4", "lxml"],
+"pymorphy2_dicts", "pymorphy2", "bs4", "lxml", "simplemma", "googletrans"],
                      "excludes": ["tkinter"]}
 
 # base="Win32GUI" should be used only for Windows GUI app
