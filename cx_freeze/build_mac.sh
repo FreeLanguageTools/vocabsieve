81615ea637f94d394199d3c5e3dd2e476d70949a
zandimna <zandimn6174@gmail.com>
2021-07-27 15:45:37 +0800

    Fix building on mac

--- cx_freeze/build_mac.sh
+++ cx_freeze/build_mac.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-python3 setup.py bdist_mac --iconfile icon.icns --bundle-name=ssmtool --custom-info-plist Info.plist
+rm -rf build
+python3 setup.py --quiet bdist_mac --iconfile icon.icns --bundle-name=ssmtool --custom-info-plist Info.plist
 cd build
 create-dmg "ssmtool.app"
