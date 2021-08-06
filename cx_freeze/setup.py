34789f4300f04d2360cb7e2e7af9c08ed1dcf4f3
1over137 <1over137@anche.no>
2021-08-06 20:31:10 +0800

    Fix deps

--- cx_freeze/setup.py
+++ cx_freeze/setup.py
@@ -5,7 +5,8 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
 build_exe_options = {"packages": ["ssmtool", "setuptools", "PyQt5",
-                                  "bs4", "lxml", "simplemma", "googletrans"],
+                                  "bs4", "lxml", "simplemma", "googletrans",
+                                  "bidict", "pystardict"],
                      "excludes": ["tkinter"]}
 
 # base="Win32GUI" should be used only for Windows GUI app
