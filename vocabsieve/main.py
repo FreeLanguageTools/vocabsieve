7658031a042544f988e0b21486f955fdfb80afbf
1over137 <1over137@anche.no>
2022-12-28 16:41:57 +0800

    Improve about window

--- vocabsieve/main.py
+++ vocabsieve/main.py
@@ -2,6 +2,7 @@ import os
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
+from PySide6 import __version__ as qtversion
 from typing import Optional
 from . import __version__
 from .app_text import *
@@ -1032,7 +1033,7 @@ class AboutDialog(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("HELLO!")
+        self.setWindowTitle("About VocabSieve")
 
         QBtn = QDialogButtonBox.Ok
 
@@ -1041,13 +1042,16 @@ class AboutDialog(QDialog):
 
         self.layout = QVBoxLayout()
         message = QLabel(
-            '''
+            f'''
+VocabSieve version: {__version__}<br>
+Python version: {sys.version}<br>
+PySide6 (Qt bindings) version: {qtversion}<br><br>
 © 2022 FreeLanguageTools<br><br>
-Visit <a href="https://freelanguagetools.org">FreeLanguageTools.org</a> for more info on how to use this tool.<br>
+Visit <a href="https://wiki.freelanguagetools.org">FreeLanguageTools Wiki</a> for more info on how to use this tool.<br>
 You can also talk to us on <a href="https://webchat.kde.org/#/room/#flt:midov.pl">Matrix</a>
 or <a href="https://t.me/fltchat">Telegram</a> for support.<br><br>
 
-Consult <a href="https://freelanguagetools.org/2021/08/dictionaries-and-frequency-lists-for-ssm/">this link</a>
+Consult <a href="https://wiki.freelanguagetools.org/vocabsieve_dicts">this wiki page</a>
 to find compatible dictionaries. <br><br>
 
 VocabSieve (formerly SSM, ssmtool) is free software available to you under the terms of
@@ -1056,10 +1060,9 @@ If you found a bug, or have enhancement ideas, please open an issue on the
 Github <a href=https://github.com/FreeLanguageTools/vocabsieve>repository</a>.<br><br>
 
 This program is yours to keep. There is no EULA you need to agree to.
-No data is sent to any server other than the configured dictionary APIs.
+No usage data is sent to any server other than the configured dictionary APIs.
 Statistics data are stored locally.
 <br><br>
-Credits: <br><a href="https://en.wiktionary.org/wiki/Wiktionary:Main_Page">Wiktionary API</a><br>
 If you find this tool useful, you can give it a star on Github and tell others about it. Any suggestions will also be appreciated.
             '''
         )
