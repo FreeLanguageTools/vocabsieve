abb66de100d93850007f970338a96e74355643cc
1over137 <1over137@anche.no>
2023-02-22 15:20:30 +0800

    Fix entrypoint scripts for cxfreeze

--- cx_freeze/win_debug/app_debug.py
+++ cx_freeze/win_debug/app_debug.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python3
-import sys
-from PyQt5.QtWidgets import QApplication
-
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    app.setApplicationName("VocabSieve")
-    app.setOrganizationName("FreeLanguageTools")
-    from vocabsieve.main import DictionaryWindow
-    w = DictionaryWindow()
-    w.show()
-    sys.exit(app.exec())
+    from vocabsieve.main import main
+    main()
