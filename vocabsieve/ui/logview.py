a8ac33e143d9cccb9e3d2add3999f62a54f1d31a
1over137 <1over137@anche.no>
2024-01-09 15:45:05 +0800

    Log config change and use monospace font

--- vocabsieve/ui/logview.py
+++ vocabsieve/ui/logview.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QDialog, QApplication, QVBoxLayout, QLabel, QWidget, QTextEdit, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QVBoxLayout, QLabel, QWidget, QPlainTextEdit, QPushButton, QHBoxLayout
 from PyQt5.QtCore import Qt, QT_VERSION_STR, PYQT_VERSION_STR
+from PyQt5.QtGui import QFont
 import sys
 
 from ..global_names import session_logs
@@ -22,17 +23,25 @@ class LogView(QDialog):
         button_box_layout = QHBoxLayout(button_box)
         button_box_layout.addWidget(btn_copy)
         button_box_layout.addWidget(btn_dismiss)
+
+        warn_message = QLabel("Be careful to check for sensitive information before sharing any logs!")
+        
         self._layout = QVBoxLayout()
+        
         message = f'''VocabSieve version: {__version__}
 Python version: {sys.version}
-PyQt5 (Qt bindings) version: {QT_VERSION_STR}, Qt {PYQT_VERSION_STR}\n\n'''
+PyQt5 (Qt bindings) version: {PYQT_VERSION_STR}, Qt {QT_VERSION_STR}\n\n'''
         message_label = QLabel(message)
         message_label.setTextFormat(Qt.PlainText)
         message_label.setOpenExternalLinks(True)
         message_label.setWordWrap(True)
         message_label.adjustSize()
 
-        log_textedit = QTextEdit()
+        log_textedit = QPlainTextEdit()
+        font = log_textedit.font()
+        font.setFamily("foobar") # non-existent font to use default mono font
+        font.setStyleHint(QFont.Monospace)
+        log_textedit.setFont(font)
         log_textedit.setReadOnly(True)
         log_textedit.setPlainText(message + session_logs.getvalue())
         btn_copy.clicked.connect(lambda: QApplication.clipboard().setText(
@@ -40,6 +49,7 @@ PyQt5 (Qt bindings) version: {QT_VERSION_STR}, Qt {PYQT_VERSION_STR}\n\n'''
             )
         )
         self._layout.addWidget(message_label)
+        self._layout.addWidget(warn_message)
         self._layout.addWidget(log_textedit)
         self._layout.addWidget(button_box)
         self.setLayout(self._layout)
