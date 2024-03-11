e437161fe4183c1630f57dcac73a82066d75110a
1over137 <dev@notyourcomputer.net>
2024-03-11 17:45:02 -0400

    Unify build scripts

--- cx_freeze/app.py
+++ cx_freeze/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-
+import os
 
 class dummyStream:
     ''' dummyStream behaves like a stream but does nothing. '''
@@ -11,14 +11,14 @@ class dummyStream:
     def flush(self): pass
     def close(self): pass
 
-
-# redirect all streams to dummy to stop error on stdout
-sys.stdout = dummyStream()
-sys.stderr = dummyStream()
-sys.stdin = dummyStream()
-sys.__stdout__ = dummyStream()
-sys.__stderr__ = dummyStream()
-sys.__stdin__ = dummyStream()
+if os.environ.get('VOCABSIEVE_DEBUG_BUILD'):
+    # redirect all streams to dummy to stop error on stdout
+    sys.stdout = dummyStream()
+    sys.stderr = dummyStream()
+    sys.stdin = dummyStream()
+    sys.__stdout__ = dummyStream()
+    sys.__stderr__ = dummyStream()
+    sys.__stdin__ = dummyStream()
 from PyQt5.QtWidgets import QApplication
 
 if __name__ == "__main__":
--- cx_freeze/setup.py
+++ cx_freeze/setup.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from vocabsieve import __version__
 from cx_Freeze import setup, Executable
 
@@ -9,6 +10,12 @@ include_files = [
      'lib/vocabsieve/reader/templates/'),
     ('../../vocabsieve/reader/static/',
      'lib/vocabsieve/reader/static/')]
+
+PYNPUT_PLATFORM_SPECIFIC_MODULES = {
+    "win32": ["pynput.keyboard._win32", "pynput.mouse._win32"],
+    "darwin": ["pynput.keyboard._darwin", "pynput.mouse._darwin"]
+}
+
 build_exe_options = {
     "includes": [
         "vocabsieve",
@@ -30,15 +37,14 @@ build_exe_options = {
         "slpp",
         "ebooklib",
         "markdown",
+        "requests",
         "markdownify",
         "lzo",
         "readmdict",
         "packaging",
         "pynput",
-        "pynput.keyboard._win32",
-        "pynput.mouse._win32",
         "gevent"
-        ],
+        ] + PYNPUT_PLATFORM_SPECIFIC_MODULES.get(sys.platform, []),
     "include_files": include_files,
     "zip_include_packages": ["PyQt5"],
     "excludes": ["tkinter"],
@@ -49,18 +55,38 @@ build_exe_options = {
 
 bdist_msi_options = {
     "upgrade_code": "{F10E2AE2-7629-3CA2-AA85-498478E708D7}",
-    "target_name": f"VocabSieve-v{__version__}-DEBUG-win64.msi"
+    "target_name": f"VocabSieve-v{__version__}-win64.msi" if not os.environ.get("VOCABSIEVE_DEBUG_BUILD") else f"VocabSieve-v{__version__}-DEBUG-win64.msi"
     }
+
+bdist_mac_options = {
+    'iconfile': "../icon.icns",
+    'bundle_name': f"VocabSieve-v{__version__}-macos",
+    'custom_info_plist': '../Info.plist',
+}
+
+bdist_dmg_options = {
+    'volume_label': f"VocabSieve-v{__version__}-macos",
+    'applications_shortcut': True,
+
+}
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
 
 setup(
-    name="VocabSieve-DEBUG",
+    name="VocabSieve",
     version=__version__,
-    description="A simple sentence mining tool",
-    options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
-    executables=[Executable("app_debug.py",
+    description="Anki companion for language learning",
+    options={
+        "build_exe": build_exe_options, 
+        "bdist_msi": bdist_msi_options, 
+        "bdist_mac": bdist_mac_options, 
+        "bdist_dmg": bdist_dmg_options
+    },
+    executables=[Executable("app.py",
                             base=base,
                             icon="../icon.ico",
                             shortcut_name="VocabSieve",
