c6282834a7d17a45b32b390c174e68c7fea59b27
1over137 <dev@notyourcomputer.net>
2024-03-23 14:16:55 -0400

    Use kaikki.org for Wiktionary (English) source

--- vocabsieve/sources/wiktionary_source.py
+++ vocabsieve/sources/wiktionary_source.py
@@ -2,18 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from ..models import DictionarySource, SourceOptions, LookupResult
 from loguru import logger
+import json
 from ..cached_get import cached_get
-
-
-def fmt_result(definitions):
-    "Format the result of dictionary lookup"
-    lines = []
-    for defn in definitions:
-        if defn['pos'] != "":
-            lines.append("<i>" + defn['pos'] + "</i>")
-        lines.extend([str(item[0] + 1) + ". " + item[1]
-                     for item in list(enumerate(defn['meaning']))])
-    return "<br>".join(lines)
+from ..dictformats import kaikki_line_to_textdef
+from ..constants import langcodes
 
 
 class WiktionarySource(DictionarySource):
@@ -26,26 +18,21 @@ class WiktionarySource(DictionarySource):
 
     def _lookup(self, word: str) -> LookupResult:
         logger.info(f"Looking up {word} in Wiktionary")
+        language_longname = langcodes[self.langcode]
         try:
             res = cached_get(
-                'https://en.wiktionary.org/api/rest_v1/page/definition/' +
-                word)
+                f"https://kaikki.org/dictionary/{language_longname}/meaning/{word[0]}/{word[0:2]}/{word}.json")
         except Exception as e:
             logger.error(f"Failed to get data from Wiktionary: {repr(e)}")
             return LookupResult(error=str(e))
         if res.status_code != 200:
             return LookupResult(error=str(res.text))
-        definitions = []
-        data = res.json()
-        if self.langcode not in data.keys():
-            return LookupResult(error="Word not defined in language")
-        data = data[self.langcode]
-        for item in data:
-            meanings = []
-            for defn in item['definitions']:
-                parsed_meaning = BeautifulSoup(defn['definition'], features="lxml")
-                meanings.append(parsed_meaning.text)
-
-            meaning_item = {"pos": item['partOfSpeech'], "meaning": meanings}
-            definitions.append(meaning_item)
-        return LookupResult(definition=fmt_result(definitions))
+        datalines = res.content.decode('utf-8').splitlines()
+        items: list[str] = []
+        for line in datalines:
+            data = json.loads(line)
+            items.append(kaikki_line_to_textdef(data))
+        if items:
+            return LookupResult(definition="\n\n".join(items))
+        else:
+            return LookupResult(error="No definitions found")
